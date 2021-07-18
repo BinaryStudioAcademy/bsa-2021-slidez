5e83a3d4b293d1770f9b4753b10416ca832f8c9f
Kirill Lesohorskyi <lesogor.kirill@gmail.com>
2021-07-18 15:10:22 +0300

    [Project Setup] Initial commit

---
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/SlidezApplication.java
@@ -0,0 +1,13 @@
+package com.binarystudio.academy.slidez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlidezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlidezApplication.class, args);
+	}
+
+}
---
+++ slidez-core/src/test/java/com/binarystudio/academy/slidez/SlidezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binarystudio.academy.slidez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlidezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
