8d374f6c98d8638fd456a7add38289287a71375d
Kirill Lesohorskyi <lesogor.kirill@gmail.com>
2021-07-24 13:53:07 +0300

    [Setup]: Added spring-security config

---
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/infrastructure/security/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.binarystudio.academy.slidez.infrastructure.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private void applyRouteRestrictions(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                // PUBLIC
+                .antMatchers("/auth/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/*").permitAll();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and()
+                .csrf().disable()
+                .httpBasic().disable()
+                .formLogin().disable()
+
+                // Set session management to stateless
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        applyRouteRestrictions(http);
+    }
+
+}
