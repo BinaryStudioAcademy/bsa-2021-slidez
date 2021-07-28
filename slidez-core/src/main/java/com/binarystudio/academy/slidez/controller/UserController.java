8a6fdeda49765c39770fba061b78a87d2cc569bd
DimaKosiczenko <dmytro.kosiczenko@gmail.com>
2021-07-28 11:27:30 +0300

    Create branch

---
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.binarystudio.academy.slidez.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+}
