1f894a00ea5fb2841c74c52417573f27807c3d2e
VladyslavArzamastsev <arzik66677@gmail.com>
2021-07-29 15:33:55 +0300

    Fixed issue with forbidden unsafe methods

--- slidez-core/src/main/java/com/binarystudio/academy/slidez/infrastructure/security/SecurityConfig.java
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/infrastructure/security/SecurityConfig.java
@@ -1,18 +1,11 @@
 package com.binarystudio.academy.slidez.infrastructure.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -28,6 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .httpBasic().disable()
                 .formLogin().disable()
 
