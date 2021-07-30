b32de06b9794b6242e9eafcf7cb4604ce4d496a7
VladyslavArzamastsev <arzik66677@gmail.com>
2021-07-30 19:39:57 +0300

    Set allowed origins to "all origins"

--- slidez-core/src/main/java/com/binarystudio/academy/slidez/infrastructure/ws/WebSocketConfig.java
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/infrastructure/ws/WebSocketConfig.java
@@ -18,7 +18,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/ws")
+            .setAllowedOrigins("*")
+            .withSockJS();
     }
 
 }
