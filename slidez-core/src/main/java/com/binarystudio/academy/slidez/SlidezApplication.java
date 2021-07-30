b63d83ec57e5a754da923a18a1608af40c2bef89
VolodymyrYavorskyi <83591768+VolodymyrYavorskyi@users.noreply.github.com>
2021-07-30 18:06:37 +0300

    test

--- slidez-core/src/main/java/com/binarystudio/academy/slidez/SlidezApplication.java
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/SlidezApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class SlidezApplication {
-
+    BLABLABLA
 	public static void main(String[] args) {
 		SpringApplication.run(SlidezApplication.class, args);
 	}
