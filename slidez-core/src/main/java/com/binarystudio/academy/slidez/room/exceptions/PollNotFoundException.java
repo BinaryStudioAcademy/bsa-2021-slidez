30e71b86b9e55daf6de961ad240cfe09b897e9f9
Maxim <skorokhodov.maxim.444@gmail.com>
2021-08-02 21:30:07 +0300

    add room for presentation

---
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/room/exceptions/PollNotFoundException.java
@@ -0,0 +1,12 @@
+package com.binarystudio.academy.slidez.room.exceptions;
+
+public class PollNotFoundException extends RuntimeException {
+
+    public PollNotFoundException() {
+        super();
+    }
+
+    public PollNotFoundException(String message) {
+        super(message);
+    }
+}
