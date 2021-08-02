f1d3e5c5acec79b61c885c989b0c9005ece6a78c
VladyslavArzamastsev <arzik66677@gmail.com>
2021-08-02 10:12:34 +0300

    Removed boilerplate by using Lombok project

--- slidez-core/src/main/java/com/binarystudio/academy/slidez/infrastructure/ws/Greeting.java
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/infrastructure/ws/Greeting.java
@@ -1,18 +1,14 @@
 package com.binarystudio.academy.slidez.infrastructure.ws;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Greeting {
 
     private String content;
-
-    public Greeting() {
-    }
-
-    public Greeting(String content) {
-        this.content = content;
-    }
-
-    public String getContent() {
-        return content;
-    }
 
 }
--- slidez-core/src/main/java/com/binarystudio/academy/slidez/infrastructure/ws/HelloMessage.java
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/infrastructure/ws/HelloMessage.java
@@ -1,21 +1,16 @@
 package com.binarystudio.academy.slidez.infrastructure.ws;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class HelloMessage {
 
     private String name;
 
-    public HelloMessage() {
-    }
-
-    public HelloMessage(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
