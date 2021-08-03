6f4fb191487dd371ac7fbcd1e7b803872b7d4279
NDMyroshnychenko <n.d.myroshnychenko@gmail.com>
2021-08-03 12:22:03 +0300

    formatted code

--- slidez-core/src/main/resources/db/migration/V2__create_link_table.sql
+++ slidez-core/src/main/resources/db/migration/V2__create_link_table.sql
@@ -1,7 +1,8 @@
-create table link (
-                      link_id uuid not null,
-                      session_id int8,
-                      link varchar(255),
-                      expiration_date TIMESTAMP WITHOUT TIME ZONE,
-                        primary key (link_id)
+create table link
+(
+    link_id         uuid not null,
+    session_id      int8,
+    link            varchar(255),
+    expiration_date TIMESTAMP WITHOUT TIME ZONE,
+    primary key (link_id)
 );
