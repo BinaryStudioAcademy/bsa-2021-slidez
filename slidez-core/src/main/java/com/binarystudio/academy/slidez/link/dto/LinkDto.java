3477ef3e5a0b6da31f16c682c12a1c189b229cca
NDMyroshnychenko <n.d.myroshnychenko@gmail.com>
2021-08-03 11:57:50 +0300

    add linkDto

---
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/link/dto/LinkDto.java
@@ -0,0 +1,35 @@
+package com.binarystudio.academy.slidez.link.dto;
+
+import com.binarystudio.academy.slidez.link.model.Link;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LinkDto {
+
+    private UUID linkId;
+
+    private Long sessionId;
+
+    private String link;
+
+    private LocalDateTime expirationDate;
+
+    public static LinkDto fromEntity(Link link) {
+        return LinkDto
+                .builder()
+                .linkId(link.getLinkId())
+                .sessionId(link.getSessionId())
+                .link(link.getLink())
+                .expirationDate(link.getExpirationDate())
+                .build();
+    }
+}
