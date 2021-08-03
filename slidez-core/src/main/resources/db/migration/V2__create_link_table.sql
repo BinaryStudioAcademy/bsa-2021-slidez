642ca69688ba2ad87980cae10af2e87cdf5b79de
NDMyroshnychenko <n.d.myroshnychenko@gmail.com>
2021-08-03 11:51:06 +0300

    add migration create table "Link"

---
+++ slidez-core/src/main/resources/db/migration/V2__create_link_table.sql
@@ -0,0 +1,7 @@
+create table link (
+                      link_id uuid not null,
+                      session_id int8,
+                      link varchar(255),
+                      expiration_date TIMESTAMP WITHOUT TIME ZONE,
+                        primary key (link_id)
+);
