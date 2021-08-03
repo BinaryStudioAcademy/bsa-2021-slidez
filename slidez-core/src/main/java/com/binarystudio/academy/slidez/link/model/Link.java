ed1c879d478815461e58ad886760916db3117d91
NDMyroshnychenko <n.d.myroshnychenko@gmail.com>
2021-08-03 11:56:59 +0300

    add entity link

---
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/link/model/Link.java
@@ -0,0 +1,36 @@
+package com.binarystudio.academy.slidez.link.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "link")
+public class Link {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "link_id", updatable = false, nullable = false)
+    private UUID linkId;
+
+    @Column(name = "session_id")
+    private Long sessionId;
+
+    @Column(name = "link")
+    private String link;
+
+    @Column(name = "expiration_date")
+    private LocalDateTime expirationDate;
+
+}
