d29fbf1a3a81a47b7421a740b058da65854458a1
NDMyroshnychenko <n.d.myroshnychenko@gmail.com>
2021-08-03 12:00:11 +0300

    add IncorrectLeaseDurationException

---
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/link/exceptions/IncorrectLeaseDurationException.java
@@ -0,0 +1,7 @@
+package com.binarystudio.academy.slidez.link.exceptions;
+
+public class IncorrectLeaseDurationException extends Exception{
+    public IncorrectLeaseDurationException(String message) {
+        super(message);
+    }
+}
