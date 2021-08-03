ed40d31ab5f8ebd34ff1a2a8780a915d9cea72de
alex-tiutiunnyk <tytynnik6377@gmail.com>
2021-08-03 12:09:31 +0300

    Refactor dsn key in backend

--- slidez-core/src/main/java/com/binarystudio/academy/slidez/SlidezApplication.java
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/SlidezApplication.java
@@ -1,13 +1,14 @@
 package com.binarystudio.academy.slidez;
 
+import io.sentry.Sentry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.sentry.spring.EnableSentry;
 
 @SpringBootApplication
-@EnableSentry(dsn = "https://b579813c296046399a14368064320494@o938867.ingest.sentry.io/5888744")
-@SuppressWarnings({"FinalClassCheck", "HideUtilityClassConstructor"}) class SlidezApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SlidezApplication.class, args);
-	}
+@SuppressWarnings({"FinalClassCheck", "HideUtilityClassConstructor"})
+class SlidezApplication {
+    public static void main(String[] args) {
+        Sentry.init();
+        SpringApplication.run(SlidezApplication.class, args);
+    }
 }
