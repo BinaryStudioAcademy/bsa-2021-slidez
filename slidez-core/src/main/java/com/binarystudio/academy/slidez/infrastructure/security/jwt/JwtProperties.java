f9b924866774f68218d30c02a3c259788d9f7a00
DimaKosiczenko <83491348+DimaKosiczenko@users.noreply.github.com>
2021-08-03 12:04:24 +0300

    Resolved pr conflicts Add configuration fields annotation

--- slidez-core/src/main/java/com/binarystudio/academy/slidez/infrastructure/security/jwt/JwtProperties.java
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/infrastructure/security/jwt/JwtProperties.java
@@ -2,6 +2,7 @@ package com.binarystudio.academy.slidez.infrastructure.security.jwt;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,8 +14,10 @@ public class JwtProperties {
 
 	private String secret;
 
+	@Value("secs_to_expire_access")
 	private Long secondsToExpireAccess;
 
+	@Value("secs_to_expire_refresh")
 	private Long secondsToExpireRefresh;
 
 }
