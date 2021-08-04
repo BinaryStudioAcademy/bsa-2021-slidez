b7042b8f8a06a5fd14302272abe99f8e60babb0b
VladyslavArzamastsev <arzik66677@gmail.com>
2021-08-04 13:52:57 +0300

    Added ability to verify token on backend with Google client library

--- slidez-core/src/main/java/com/binarystudio/academy/slidez/infrastructure/security/oauth2/OAuth2Properties.java
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/infrastructure/security/oauth2/OAuth2Properties.java
@@ -13,4 +13,6 @@ public class OAuth2Properties {
 
 	private String redirectUri;
 
+	private String clientId;
+
 }
--- slidez-core/src/main/java/com/binarystudio/academy/slidez/infrastructure/security/oauth2/OAuthController.java
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/infrastructure/security/oauth2/OAuthController.java
@@ -1,19 +1,37 @@
 package com.binarystudio.academy.slidez.infrastructure.security.oauth2;
 
 import com.binarystudio.academy.slidez.infrastructure.security.auth.model.AuthorizationByTokenRequest;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 @RestController
 @RequestMapping("/auth")
 public class OAuthController {
 
-	@PostMapping("/login/google")
+    private final GoogleTokenVerifier googleTokenVerifier;
+
+    @Autowired
+    public OAuthController(GoogleTokenVerifier googleTokenVerifier) {
+        this.googleTokenVerifier = googleTokenVerifier;
+    }
+
+    @PostMapping("/login/google")
 	public void loginGoogle(@RequestBody AuthorizationByTokenRequest authorizationByTokenRequest) {
-        System.out.println("Login: ");
         System.out.println(authorizationByTokenRequest);
+        try {
+            GoogleIdToken.Payload verify = this.googleTokenVerifier.verify(authorizationByTokenRequest.getToken());
+            System.out.println(verify.getEmail());
+        }
+        catch (GeneralSecurityException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
