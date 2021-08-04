e04089753aa3e68e1bbb794eb0136d42de5b039d
NDMyroshnychenko <n.d.myroshnychenko@gmail.com>
2021-08-04 15:32:26 +0300

    change type link_id and session_id

--- slidez-core/src/main/resources/db/migration/V2__create_link_table.sql
+++ slidez-core/src/main/resources/db/migration/V2__create_link_table.sql
@@ -1,7 +1,7 @@
 create table link
 (
-    link_id         uuid not null,
-    session_id      int8,
+    link_id         bigint not null,
+    session_id      uuid,
     link            varchar(255),
     expiration_date TIMESTAMP WITHOUT TIME ZONE,
     primary key (link_id)
