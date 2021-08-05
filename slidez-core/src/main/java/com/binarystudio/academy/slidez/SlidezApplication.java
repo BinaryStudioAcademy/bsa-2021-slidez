f0da69f159f08707cdbad728866ed4a821e62584
Anatolii Ivanov <anatolii-ivanov@binary-studio.com>
2021-08-05 21:52:29 +0300

    Let it be so

--- slidez-core/src/main/java/com/binarystudio/academy/slidez/SlidezApplication.java
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/SlidezApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@SuppressWarnings({ "HideUtilityClassConstructor" })
+@SuppressWarnings({ "FinalClassCheck", "HideUtilityClassConstructor" })
 class SlidezApplication {
 
 	public static void main(String[] args) {
