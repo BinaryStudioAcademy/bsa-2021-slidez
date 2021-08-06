23d6c856ef1ca617c05126c86268d799e1a36d09
Kirill Lesohorskyi <lesogor.kirill@gmail.com>
2021-08-06 16:41:35 +0300

    [HOTFIX] Open swagger path in spring security (#58)

--- scripts/run-infrastructure.sh
+++ scripts/run-infrastructure.sh
@@ -1,1 +1,1 @@
-docker run  --name=slidez-postgres -v pg-slidez-volume:/var/lib/postgresql/data -p 5432:5432 -e POSTGRES_PASSWORD=secret postgres:latest
+docker run  --name=slidez-postgres -v pg-slidez-volume:/var/lib/postgresql/data -p 5432:5432 -e POSTGRES_PASSWORD=secret postgres:13.3
