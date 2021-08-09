75d9e05f46c15a9af895c2fafe96f4c072631f77
VladyslavArzamastsev <arzik66677@gmail.com>
2021-08-09 17:18:49 +0300

    Attempt to fix google oauth #1: added @Value on oauth properties

--- slidez-core/src/main/java/com/binarystudio/academy/slidez/infrastructure/security/oauth2/OAuth2Properties.java
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/infrastructure/security/oauth2/OAuth2Properties.java
@@ -2,6 +2,7 @@ package com.binarystudio.academy.slidez.infrastructure.security.oauth2;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 @Setter
 public class OAuth2Properties {
 
+	@Value("#{'${oauth2.client-id}'}")
 	private String clientId;
 
 }
