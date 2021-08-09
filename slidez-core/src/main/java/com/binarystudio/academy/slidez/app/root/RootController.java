ef0b19f28bbd2a5e9b1ade5b631e4d8cdff3729c
DimaKosiczenko <83491348+DimaKosiczenko@users.noreply.github.com>
2021-08-09 23:22:41 +0300

    RootController revert to develop branch

--- slidez-core/src/main/java/com/binarystudio/academy/slidez/app/root/RootController.java
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/app/root/RootController.java
@@ -1,7 +1,5 @@
 package com.binarystudio.academy.slidez.app.root;
 
-import com.binarystudio.academy.slidez.domain.session.SessionService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class RootController {
 
-	@Autowired
-	SessionService service;
-
 	@GetMapping
 	public String helloSlidez() {
-
 		return "BSA-2021-slidez v0.0.1";
-
 	}
 
 }
