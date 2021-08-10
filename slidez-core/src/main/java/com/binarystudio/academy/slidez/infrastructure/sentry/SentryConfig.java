8d03bd4bd88a3306caafdd3e50044855c520a6ca
Kirill Lesohorskyi <lesogor.kirill@gmail.com>
2021-08-10 15:37:39 +0300

    [HOTFIX]: Disabled sentry

--- slidez-core/src/main/java/com/binarystudio/academy/slidez/infrastructure/sentry/SentryConfig.java
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/infrastructure/sentry/SentryConfig.java
@@ -1,10 +1,8 @@
 package com.binarystudio.academy.slidez.infrastructure.sentry;
 
-import io.sentry.spring.EnableSentry;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableSentry()
 public class SentryConfig {
 
 }
