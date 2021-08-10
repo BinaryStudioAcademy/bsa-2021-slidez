7e3dc6ea36a86cc96790021b6b966a0d8230478d
Kirill Lesohorskyi <lesogor.kirill@gmail.com>
2021-08-10 15:21:55 +0300

    [HOTFIX]: Swiched to spring sentry integration

--- slidez-core/src/main/java/com/binarystudio/academy/slidez/infrastructure/sentry/SentryConfig.java
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/infrastructure/sentry/SentryConfig.java
@@ -1,33 +1,10 @@
 package com.binarystudio.academy.slidez.infrastructure.sentry;
 
-import io.sentry.Sentry;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import io.sentry.spring.EnableSentry;
+import org.springframework.context.annotation.Configuration;
 
-@Component
-@Log4j2
-public class SentryConfig implements InitializingBean {
-
-	@Value("${sentry.dsn:#{null}}")
-	private String sentryDsn;
-
-	@Value("${sentry.in-app-includes:#{null}}")
-	private String inAppIncludes;
-
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		if (this.sentryDsn == null) {
-			log.warn("Sentry DSN is not set, failed to initialize Sentry");
-			return;
-		}
-		if (this.inAppIncludes == null) {
-			log.warn("Sentry in-app-includes is not set, failed to initialize Sentry");
-			return;
-		}
-
-		Sentry.init();
-	}
+@Configuration
+@EnableSentry()
+public class SentryConfig {
 
 }
