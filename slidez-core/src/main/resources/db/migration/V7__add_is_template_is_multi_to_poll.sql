c5f51aee8c3fc9600eb46eb18279b4a906128af0
DimaKosiczenko <83491348+DimaKosiczenko@users.noreply.github.com>
2021-08-16 17:54:56 +0300

    Ref Flyway migration scripts

--- slidez-core/src/main/resources/db/migration/V7__add_is_template_is_multi_to_poll.sql
+++ slidez-core/src/main/resources/db/migration/V7__add_is_template_is_multi_to_poll.sql
@@ -1,6 +1,4 @@
-ALTER TABLE polls add (
-    is_multi boolean,
-    is_template boolean,
-    pie_id uuid
-    );
+ALTER TABLE polls add is_multi boolean;
+ALTER TABLE polls add is_template boolean;
+ALTER TABLE polls add pie_id uuid;
 
