df4c330e8e172270e4a3ee637d79b2fa61d4f87e
NDMyroshnychenko <n.d.myroshnychenko@gmail.com>
2021-08-16 14:15:59 +0300

    delete comments

--- slidez-fe-addon/src/client/sidebar-add-on/background.js
+++ slidez-fe-addon/src/client/sidebar-add-on/background.js
@@ -5,23 +5,10 @@ log();
 chrome.runtime.onMessage.addListener((request, sender, response) => {
   if (request.message === 'extension_connected') {
     log();
+    //test document.body.style.backgroundColor = "red"
     const { type, auth_token } = response;
-    console.log(' - type -  ' + type + '\n - auth_token - ' + auth_token);
+    //console.log(' - type -  ' + type + '\n - auth_token - ' + auth_token);
     return auth_token;
   }
 });
-
-
-// const port = chrome.runtime.connect({ name: 'extension_connected' });
-// port.postMessage({
-//   type: 'extension_connected',
-//   auth_token: 'auth_token',
-// });
-
-// chrome.runtime.connect('extension_connected', function (response: ChromeEvents) {
-//   console.log('connect - ' + response);
-//   log();
-//   const { type, auth_token } = response;
-//   console.log(type + ' ' + auth_token);
-// });
 export {}
