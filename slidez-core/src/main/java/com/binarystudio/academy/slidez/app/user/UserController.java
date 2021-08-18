59f77b008e271f93ced3ba1bacfd239371274345
alex-tiutiunnyk <tytynnik6377@gmail.com>
2021-08-18 10:38:33 +0300

    Add some methods

--- slidez-core/src/main/java/com/binarystudio/academy/slidez/app/user/UserController.java
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/app/user/UserController.java
@@ -1,52 +1,62 @@
 package com.binarystudio.academy.slidez.app.user;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import com.binarystudio.academy.slidez.domain.auth.jwtauth.model.AuthorizationByTokenRequest;
 import com.binarystudio.academy.slidez.domain.response.GenericResponse;
 import com.binarystudio.academy.slidez.domain.user.UserService;
 import com.binarystudio.academy.slidez.domain.user.dto.UserDetailsDto;
 import com.binarystudio.academy.slidez.domain.user.mapper.UserMapper;
 import com.binarystudio.academy.slidez.domain.user.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("${v1API}/users")
-//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
-	private final UserService userService;
+    private final UserService userService;
 
-	@Autowired
-	public UserController(UserService userService) {
-		this.userService = userService;
-	}
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
-	@GetMapping("{id}")
-	public GenericResponse<UserDetailsDto, UserResponseCodes> getById(@PathVariable("id") UUID id) {
-		Optional<User> userOptional = this.userService.getById(id);
-		if (userOptional.isEmpty()) {
-			return new GenericResponse<>(null, UserResponseCodes.NOT_FOUND);
-		}
+    @GetMapping("{id}")
+    public GenericResponse<UserDetailsDto, UserResponseCodes> getById(@PathVariable("id") UUID id) {
+        Optional<User> userOptional = this.userService.getById(id);
+        if (userOptional.isEmpty()) {
+            return new GenericResponse<>(null, UserResponseCodes.NOT_FOUND);
+        }
 
-		User user = userOptional.get();
-		UserDetailsDto userDetailsDto = UserMapper.INSTANCE.mapUserToUserDetailsDto(user);
-		return new GenericResponse<>(userDetailsDto, null);
-	}
+        User user = userOptional.get();
+        UserDetailsDto userDetailsDto = UserMapper.INSTANCE.mapUserToUserDetailsDto(user);
+        return new GenericResponse<>(userDetailsDto, null);
+    }
 
-   // @PostMapping("")
-   // public ResponseEntity<Object> updateUserByToken(@RequestParam("token") String token, @RequestBody UserDto userDto) {
-    //    if (token == null || token.isEmpty()) {
-     //       return new ResponseEntity<>("Invalid token", HttpStatus.BAD_REQUEST);
-      //  }
-      //  Optional<User> user = this.userService.updateUserByToken(token, userDto);
-      //  if (user.isEmpty()) {
-      //      return new ResponseEntity<>("Bad token.", HttpStatus.BAD_REQUEST);
-      //  }
-     //   return new ResponseEntity<>(user.get(), HttpStatus.OK);
-   // }
+    @GetMapping()
+    public GenericResponse<List<User>, UserResponseCodes> getAll() {
+        List<User> userOptional = this.userService.getAll();
+        if (userOptional.isEmpty()) {
+            return new GenericResponse<>(null, UserResponseCodes.NOT_FOUND);
+        }
+
+        return new GenericResponse <>(userOptional, null);
+    }
+
+    //not working
+    @GetMapping("userInfo")
+    public GenericResponse<UserDetailsDto, UserResponseCodes> getByToken(@RequestParam("token") AuthorizationByTokenRequest authorizationByTokenRequest) {
+        Optional<User> userOptional = this.userService.getByToken(authorizationByTokenRequest);
+        if(userOptional.isEmpty()) {
+            return new GenericResponse<>(null, UserResponseCodes.NOT_FOUND);
+        }
+
+        User user = userOptional.get();
+        UserDetailsDto userDetailsDto = UserMapper.INSTANCE.mapUserToUserDetailsDto(user);
+        return new GenericResponse<>(userDetailsDto, null);
+    }
 }
--- slidez-core/src/main/java/com/binarystudio/academy/slidez/domain/user/UserService.java
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/domain/user/UserService.java
@@ -1,67 +1,66 @@
 package com.binarystudio.academy.slidez.domain.user;
 
-//import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
+import com.binarystudio.academy.slidez.domain.auth.jwtauth.JwtProvider;
+import com.binarystudio.academy.slidez.domain.auth.jwtauth.model.AuthorizationByTokenRequest;
 import com.binarystudio.academy.slidez.domain.user.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserService {
 
-	private final PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
-	private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-	@Autowired
-	public UserService(PasswordEncoder passwordEncoder, UserRepository userRepository) {
-		this.passwordEncoder = passwordEncoder;
-		this.userRepository = userRepository;
-	}
+    private final JwtProvider jwtProvider;
+
+    @Autowired
+    public UserService(PasswordEncoder passwordEncoder, UserRepository userRepository, JwtProvider jwtProvider) {
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+        this.jwtProvider = jwtProvider;
+    }
 
     public boolean isEmailPresent(String email) {
-		return this.userRepository.existsByEmail(email);
-	}
+        return this.userRepository.existsByEmail(email);
+    }
 
-	public Optional<User> getByEmail(String email) {
-		return this.userRepository.findByEmail(email);
-	}
+    public Optional<User> getByEmail(String email) {
+        return this.userRepository.findByEmail(email);
+    }
 
-	public User create(String email, String password) {
-		User user = new User();
-		user.setEmail(email);
-		user.setPassword(this.passwordEncoder.encode(password));
-		return this.userRepository.save(user);
-	}
+    public User create(String email, String password) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(this.passwordEncoder.encode(password));
+        return this.userRepository.save(user);
+    }
 
     public Optional<User> getById(UUID id) {
-		return this.userRepository.findById(id);
-	}
+        return this.userRepository.findById(id);
+    }
 
-	public User createByEmail(String email) {
-		return create(email, "");
-	}
+    public User createByEmail(String email) {
+        return create(email, "");
+    }
 
-   // @Transactional
-   // public Optional<User> updateUserByToken(String token, UserDto userDto) {
-   //     String email = this.authService.getLoginFromToken(token);
-  //      if (email == null) {
-  //          return Optional.empty();
-   //     }
-   //     Optional<User> userOptional = findByEmail(email);
-   //     User updatedUser = userOptional.orElseThrow();
-    //    updatedUser.setFirstName(userDto.getFirstName());
-    //    updatedUser.setLastName(userDto.getLastName());
-    //    this.userRepository.saveAndFlush(updatedUser);
-    //    User user = UserMapper.INSTANCE.userDtoToUser(userDto);
-     //   return Optional.of(user);
-  //  }
+    public List<User> getAll() {
+        return this.userRepository.findAll();
+    }
 
-  //  public List<User> findAll() {
-  //      return this.userRepository.findAll();
-  //  }
+    public Optional<User> getByToken(AuthorizationByTokenRequest authorizationByTokenRequest) {
+        System.out.println(authorizationByTokenRequest);
+        Optional<String> email = jwtProvider.getLoginFromToken(authorizationByTokenRequest.getToken());
+        System.out.println(email);
+        if (email.isEmpty()) {
+            return Optional.empty();
+        }
+        return getByEmail(email.get());
+    }
 }
