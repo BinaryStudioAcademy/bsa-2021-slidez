fb6279c84dcc8bb12aab5bf5ef72b2a560114d7f
VolodymyrYavorskyi <83591768+VolodymyrYavorskyi@users.noreply.github.com>
2021-08-19 16:30:30 +0300

    Move poll to FE-core

--- slidez-extension/src/components/poll/dto/PollDto.ts
+++
@@ -1,8 +0,0 @@
-import { PollAnswerDto } from './PollAnswerDto'
-import { PollOptionDto } from './PollOptionDto'
-
-export type PollDto = {
-    name: string
-    options: PollOptionDto[]
-    answers: Record<number, PollAnswerDto[]>
-}
