51dba924d2dd8960da9f3e5ef47058aa28937007
NDMyroshnychenko <n.d.myroshnychenko@gmail.com>
2021-08-20 13:11:59 +0300

    fix backend linter

--- slidez-core/src/main/java/com/binarystudio/academy/slidez/domain/user/UserService.java
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/domain/user/UserService.java
@@ -14,53 +14,54 @@ import java.util.UUID;
 @Service
 public class UserService {
 
-    private final PasswordEncoder passwordEncoder;
+	private final PasswordEncoder passwordEncoder;
 
-    private final UserRepository userRepository;
+	private final UserRepository userRepository;
 
-    private final JwtProvider jwtProvider;
+	private final JwtProvider jwtProvider;
 
-    @Autowired
-    public UserService(PasswordEncoder passwordEncoder, UserRepository userRepository, JwtProvider jwtProvider) {
-        this.passwordEncoder = passwordEncoder;
-        this.userRepository = userRepository;
-        this.jwtProvider = jwtProvider;
-    }
+	@Autowired
+	public UserService(PasswordEncoder passwordEncoder, UserRepository userRepository, JwtProvider jwtProvider) {
+		this.passwordEncoder = passwordEncoder;
+		this.userRepository = userRepository;
+		this.jwtProvider = jwtProvider;
+	}
 
-    public boolean isEmailPresent(String email) {
-        return this.userRepository.existsByEmail(email);
-    }
+	public boolean isEmailPresent(String email) {
+		return this.userRepository.existsByEmail(email);
+	}
 
-    public Optional<User> getByEmail(String email) {
-        return this.userRepository.findByEmail(email);
-    }
+	public Optional<User> getByEmail(String email) {
+		return this.userRepository.findByEmail(email);
+	}
 
-    public User create(String email, String password) {
-        User user = new User();
-        user.setEmail(email);
-        user.setPassword(this.passwordEncoder.encode(password));
-        return this.userRepository.save(user);
-    }
+	public User create(String email, String password) {
+		User user = new User();
+		user.setEmail(email);
+		user.setPassword(this.passwordEncoder.encode(password));
+		return this.userRepository.save(user);
+	}
 
-    public Optional<User> getById(UUID id) {
-        return this.userRepository.findById(id);
-    }
+	public Optional<User> getById(UUID id) {
+		return this.userRepository.findById(id);
+	}
 
-    public User createByEmail(String email) {
-        return create(email, "");
-    }
+	public User createByEmail(String email) {
+		return create(email, "");
+	}
 
-    public List<User> getAll() {
-        return this.userRepository.findAll();
-    }
+	public List<User> getAll() {
+		return this.userRepository.findAll();
+	}
 
-    public Optional<User> getByToken(AuthorizationByTokenRequest authorizationByTokenRequest) {
-        System.out.println(authorizationByTokenRequest);
-        Optional<String> email = jwtProvider.getLoginFromToken(authorizationByTokenRequest.getToken());
-        System.out.println(email);
-        if (email.isEmpty()) {
-            return Optional.empty();
-        }
-        return getByEmail(email.get());
-    }
+	public Optional<User> getByToken(AuthorizationByTokenRequest authorizationByTokenRequest) {
+		System.out.println(authorizationByTokenRequest);
+		Optional<String> email = jwtProvider.getLoginFromToken(authorizationByTokenRequest.getToken());
+		System.out.println(email);
+		if (email.isEmpty()) {
+			return Optional.empty();
+		}
+		return getByEmail(email.get());
+	}
+
 }
