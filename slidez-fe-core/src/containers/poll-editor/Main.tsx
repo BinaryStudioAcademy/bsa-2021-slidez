eae973ab932c01583b618b973a98044fffa98324
alex-tiutiunnyk <tytynnik6377@gmail.com>
2021-08-23 15:41:50 +0300

    Change page style

--- slidez-fe-core/src/containers/poll-editor/Main.tsx
+++ slidez-fe-core/src/containers/poll-editor/Main.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { QandA } from '../../common/components/interactive-elements/q-and-a/QandA'
 import { Page } from './enums/addon-routes'
 import PollEditor from './PollEditor'
@@ -9,8 +9,8 @@ import q_and_a from '../../assets/svgs/QandA.svg'
 import heart from '../../assets/svgs/reactions.svg'
 import chat from '../../assets/svgs/chat.svg'
 
-const renderAddonPages = (page: Page) => {
-    switch (page) {
+const renderAddonPages = (currentPage: Page) => {
+    switch (currentPage) {
         case 'MAIN':
             return <Main />
         case 'POLL':
@@ -21,7 +21,7 @@ const renderAddonPages = (page: Page) => {
             return <Main />
     }
 }
-//подумать как менять страницу
+
 const Main = () => {
     return (
         <div className='main-page-addon'>
@@ -32,19 +32,19 @@ const Main = () => {
             <div className='menu-list'>
                 <div className='list-items'>
                     <img src={check} alt='check' />
-                    Live poll
+                    <div className='text'>Live poll</div>
                 </div>
                 <div className='list-items'>
                     <img src={q_and_a} alt='q_and_a' />
-                    Live poll
+                    <div className='text'>Audience Q&#38;A</div>
                 </div>
                 <div className='list-items'>
                     <img src={heart} alt='heart' />
-                    Live poll
+                    <div className='text'>Reactions</div>
                 </div>
                 <div className='list-items'>
                     <img src={chat} alt='chat' />
-                    Live poll
+                    <div className='text'>Quiz</div>
                 </div>
             </div>
         </div>
