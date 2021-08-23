a6a19fbce17983c7b78ccd53c322ddeea46fc9a6
Anna Prusakova <hanna.prusakova@gmail.com>
2021-08-23 13:36:30 +0300

    update sidebar icons

--- slidez-fe-core/src/pages/dashboard/SideBar.tsx
+++ slidez-fe-core/src/pages/dashboard/SideBar.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import {
-    faBook,
-    faPlus,
-    faShapes,
-    faSortAmountUpAlt,
-} from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import SlidezLogo from '../../../src/logo_Slidez_1.svg'
+import create_presentation from '../../assets/svgs/create_presentation.svg'
+import my_presentation from '../../assets/svgs/my_presentation.svg'
+import graph_icon from '../../assets/svgs/graph_icon.svg'
+import layout_icon from '../../assets/svgs/layout_icon.svg'
 
 const SideBar = () => {
     return (
@@ -20,28 +17,31 @@ const SideBar = () => {
                 <span>
                     <a href=''>
                         <div className='icon-add'>
-                            <FontAwesomeIcon icon={faPlus} />
+                            <img
+                                src={create_presentation}
+                                alt='add presentation'
+                            ></img>
                         </div>
                     </a>
                 </span>
                 <span>
                     <a href=''>
                         <div className='icon-book'>
-                            <FontAwesomeIcon icon={faBook} />
+                            <img
+                                src={my_presentation}
+                                alt='my presentations'
+                            ></img>
                         </div>
                     </a>
                 </span>
                 <span>
-                    <a href='#'>
-                        <FontAwesomeIcon
-                            className='icon'
-                            icon={faSortAmountUpAlt}
-                        />
+                    <a href=''>
+                        <img src={graph_icon} alt='graph'></img>
                     </a>
                 </span>
                 <span>
-                    <a href='#'>
-                        <FontAwesomeIcon className='icon' icon={faShapes} />
+                    <a href=''>
+                        <img src={layout_icon} alt='layout'></img>
                     </a>
                 </span>
             </div>
