d88a5007e9c38f8db52d685758c0a8a4771ad0ee
alex-tiutiunnyk <tytynnik6377@gmail.com>
2021-08-25 19:50:58 +0300

    Add event pages

---
+++ slidez-fe-core/src/pages/event/EventsPage.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import './styles.scss'
+import Header from '../participant-page/Header'
+import {
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+    Radio,
+    RadioGroup,
+} from '@material-ui/core'
+
+const Event = () => {
+    const text =
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer egestas nibh placerat fermentum?'
+    const [question, setQuestion] = useState(text)
+    return (
+        <div className='events-page'>
+            <Header eventName='Event name' />
+            <div className='page-content'>
+                <FormControl component='fieldset'>
+                    <FormLabel className='question' component='legend'>
+                        {question}
+                    </FormLabel>
+                    <RadioGroup
+                        className='radio-area'
+                        aria-label='question'
+                        name='gender1'
+                    >
+                        <FormControlLabel
+                            className='radio-item'
+                            label='Integer egestas'
+                            control={<Radio />}
+                            value='option1'
+                        />
+                        <FormControlLabel
+                            className='radio-item'
+                            label='Consectetur'
+                            control={<Radio />}
+                            value='option2'
+                        />
+                        <FormControlLabel
+                            className='radio-item'
+                            label='Faucibus eu quisque'
+                            control={<Radio />}
+                            value='option3'
+                        />
+                        <FormControlLabel
+                            className='radio-item'
+                            label='Duis elementum egestas '
+                            control={<Radio />}
+                            value='option4'
+                        />
+                    </RadioGroup>
+                </FormControl>
+                <button className='btn-submit'>Submit</button>
+                {/* create button q&a*/}
+            </div>
+        </div>
+    )
+}
+
+export default Event
---
+++ slidez-fe-core/src/pages/event/NoEventPage.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import './styles.scss'
+import Header from '../participant-page/Header'
+
+const NoEvent = () => {
+    return (
+        <div className='events-page'>
+            <Header eventName='Event name' />
+            <div className='error-content'>
+                Please wait for the presenter to show the next slide.
+            </div>
+            {/* create button q&a*/}
+        </div>
+    )
+}
+
+export default NoEvent
