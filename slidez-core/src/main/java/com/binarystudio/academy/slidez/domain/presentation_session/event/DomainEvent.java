de8b990c119aec5ece163432b4bcdd83160b854f
VladyslavArzamastsev <arzik66677@gmail.com>
2021-08-25 10:15:38 +0300

    Added necessary Jackson annotations

--- slidez-core/src/main/java/com/binarystudio/academy/slidez/domain/presentation_session/event/DomainEvent.java
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/domain/presentation_session/event/DomainEvent.java
@@ -2,12 +2,19 @@ package com.binarystudio.academy.slidez.domain.presentation_session.event;
 
 import com.binarystudio.academy.slidez.domain.exception.DomainException;
 import com.binarystudio.academy.slidez.domain.presentation_session.model.State;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes({ @JsonSubTypes.Type(value = StartPollEvent.class, name = "StartPollEvent"),
+		@JsonSubTypes.Type(value = SnapshotRequestedEvent.class, name = "SnapshotRequestedEvent"),
+		@JsonSubTypes.Type(value = AnswerPollEvent.class, name = "AnswerPollEvent") })
 public abstract class DomainEvent {
 
 	private final UUID id = UUID.randomUUID();
