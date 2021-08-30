a85b80d3c8a8978ab4ac102ad6f9272777debd41
NDMyroshnychenko <n.d.myroshnychenko@gmail.com>
2021-08-30 23:19:50 +0300

    add wait from fetching data

--- slidez-fe-core/src/pages/dashboard/Dashboard.tsx
+++ slidez-fe-core/src/pages/dashboard/Dashboard.tsx
@@ -12,9 +12,17 @@ import {
 import SideBar from './SideBar'
 import './dashboard.scss'
 import { MOCK_DATA } from './mock-data'
-import { useAppDispatch } from '../../hooks'
+import { useAppDispatch, useAppSelector } from '../../hooks'
 import table_sort from '../../../src/assets/svgs/table-sort.svg'
 import UserProfile from './user-profile/UserProfile'
+import {
+    isFetchingUser,
+    loginByToken,
+    selectIsLoggedIn,
+} from '../../containers/user/store'
+import { TokenDto } from '../../services/auth/dto/TokenDto'
+import Loader from '../../common/components/loader/Loader'
+import '../../common/components/loader/Loader.css'
 
 const Dashboard = () => {
     const [currentView, setCurrentView] = useState('table')
@@ -23,6 +31,23 @@ const Dashboard = () => {
     const [filteredPresentations, setFilteredPresentations] =
         useState(MOCK_DATA)
     const dispatch = useAppDispatch()
+    const isFetchingUserData = useAppSelector(isFetchingUser)
+    const isLoggedIn = useAppSelector(selectIsLoggedIn)
+    const JWT = 'jwt'
+    const refreshJWT = 'refresh_jwt'
+    const [token, setToken] = useState('')
+
+    useEffect(() => {
+        console.log(isFetchingUserData)
+        setToken(window.localStorage.getItem(JWT) || '')
+    })
+
+    const handleToken = (token: string) => {
+        const dto: TokenDto = {
+            token: token,
+        }
+        dispatch(loginByToken(dto))
+    }
 
     const handleToggleCurrentView = useCallback(() => {
         setCurrentView((view) => (view === 'table' ? 'grid' : 'table'))
@@ -111,130 +136,135 @@ const Dashboard = () => {
         setSearchField(e.target.value)
     }
 
-    return (
-        <div className='dashboard-page'>
-            <SideBar />
-            <div className='page-content'>
-                <div className='search-and-user'>
-                    <div className='search-bar'>
-                        <FontAwesomeIcon
-                            className='search-icon'
-                            icon={faSearch}
-                        />
-                        <input
-                            id='searchQueryInput'
-                            type='search'
-                            placeholder='Search...'
-                            onChange={handleChange}
-                        />
+    if (!isFetchingUserData) {
+        handleToken(token)
+        return <Loader />
+    } else {
+        return (
+            <div className='dashboard-page'>
+                <SideBar />
+                <div className='page-content'>
+                    <div className='search-and-user'>
+                        <div className='search-bar'>
+                            <FontAwesomeIcon
+                                className='search-icon'
+                                icon={faSearch}
+                            />
+                            <input
+                                id='searchQueryInput'
+                                type='search'
+                                placeholder='Search...'
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <UserProfile />
                     </div>
-                    <UserProfile />
-                </div>
-                <div className='presentations-section'>
-                    <div className='above-section'>
-                        <p>Your presentation</p>
-                        <div className='table-grid-toggler'>
-                            <div>
-                                <label>
-                                    <input
-                                        type='radio'
-                                        value='table'
-                                        checked={currentView === 'grid'}
-                                        onChange={handleToggleCurrentView}
-                                        onClick={handleClickedButton}
-                                    />
-                                    <FontAwesomeIcon
-                                        icon={faList}
-                                        className={
-                                            currentView === 'grid'
-                                                ? 'view-btn-active'
-                                                : 'view-btn'
-                                        }
-                                    />
-                                </label>
-                            </div>
-                            <div className='verticalLine' />
-                            <div>
-                                <label>
-                                    <input
-                                        type='radio'
-                                        value='grid'
-                                        checked={currentView === 'table'}
-                                        onChange={handleToggleCurrentView}
-                                        onClick={handleClickedButton}
-                                    />
-                                    <FontAwesomeIcon
-                                        icon={faThLarge}
-                                        className={
-                                            currentView === 'table'
-                                                ? 'view-btn-active'
-                                                : 'view-btn'
-                                        }
-                                    />
-                                </label>
+                    <div className='presentations-section'>
+                        <div className='above-section'>
+                            <p>Your presentation</p>
+                            <div className='table-grid-toggler'>
+                                <div>
+                                    <label>
+                                        <input
+                                            type='radio'
+                                            value='table'
+                                            checked={currentView === 'grid'}
+                                            onChange={handleToggleCurrentView}
+                                            onClick={handleClickedButton}
+                                        />
+                                        <FontAwesomeIcon
+                                            icon={faList}
+                                            className={
+                                                currentView === 'grid'
+                                                    ? 'view-btn-active'
+                                                    : 'view-btn'
+                                            }
+                                        />
+                                    </label>
+                                </div>
+                                <div className='verticalLine' />
+                                <div>
+                                    <label>
+                                        <input
+                                            type='radio'
+                                            value='grid'
+                                            checked={currentView === 'table'}
+                                            onChange={handleToggleCurrentView}
+                                            onClick={handleClickedButton}
+                                        />
+                                        <FontAwesomeIcon
+                                            icon={faThLarge}
+                                            className={
+                                                currentView === 'table'
+                                                    ? 'view-btn-active'
+                                                    : 'view-btn'
+                                            }
+                                        />
+                                    </label>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div>
-                        {currentView === 'grid' ? (
-                            <div className='table'>
-                                <table id='table-presentation'>
-                                    <tbody>
-                                        <tr>{renderTableHeader()}</tr>
-                                        {renderTableData()}
-                                    </tbody>
-                                </table>
-                                <div className='no-found-presentations'>
-                                    <span className='text-no-found'>
-                                        {isNotEmptyPresentation()}
-                                    </span>
-                                </div>
-                            </div>
-                        ) : (
-                            <div className='grid'>
-                                {filteredPresentations.map((md) => (
-                                    <div className='card' key={md.id}>
-                                        <img
-                                            className='card-img'
-                                            src={md.pictureUrl}
-                                        />
-                                        <div className='card-name'>
-                                            {md.name}
-                                        </div>
-                                        <div className='card-info'>
-                                            <span className='card-icons'>
-                                                <FontAwesomeIcon
-                                                    className='icon-presentation'
-                                                    icon={faChalkboard}
-                                                />
-                                                <FontAwesomeIcon
-                                                    className='icon-users'
-                                                    icon={faUsers}
-                                                />
-                                                <span className='card-date'>
-                                                    12.07.21
-                                                </span>
-                                            </span>
-                                            <span className='icon-details'>
-                                                <FontAwesomeIcon
-                                                    icon={faEllipsisV}
-                                                />
-                                            </span>
-                                        </div>
+                        <div>
+                            {currentView === 'grid' ? (
+                                <div className='table'>
+                                    <table id='table-presentation'>
+                                        <tbody>
+                                            <tr>{renderTableHeader()}</tr>
+                                            {renderTableData()}
+                                        </tbody>
+                                    </table>
+                                    <div className='no-found-presentations'>
+                                        <span className='text-no-found'>
+                                            {isNotEmptyPresentation()}
+                                        </span>
                                     </div>
-                                ))}
-                                <div className='no-found-presentations'>
-                                    <span className='text-no-found'>
-                                        {isNotEmptyPresentation()}
-                                    </span>
                                 </div>
-                            </div>
-                        )}
+                            ) : (
+                                <div className='grid'>
+                                    {filteredPresentations.map((md) => (
+                                        <div className='card' key={md.id}>
+                                            <img
+                                                className='card-img'
+                                                src={md.pictureUrl}
+                                            />
+                                            <div className='card-name'>
+                                                {md.name}
+                                            </div>
+                                            <div className='card-info'>
+                                                <span className='card-icons'>
+                                                    <FontAwesomeIcon
+                                                        className='icon-presentation'
+                                                        icon={faChalkboard}
+                                                    />
+                                                    <FontAwesomeIcon
+                                                        className='icon-users'
+                                                        icon={faUsers}
+                                                    />
+                                                    <span className='card-date'>
+                                                        12.07.21
+                                                    </span>
+                                                </span>
+                                                <span className='icon-details'>
+                                                    <FontAwesomeIcon
+                                                        icon={faEllipsisV}
+                                                    />
+                                                </span>
+                                            </div>
+                                        </div>
+                                    ))}
+                                    <div className='no-found-presentations'>
+                                        <span className='text-no-found'>
+                                            {isNotEmptyPresentation()}
+                                        </span>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Dashboard
