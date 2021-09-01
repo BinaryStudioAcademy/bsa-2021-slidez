59c6807e5dad3dadcdc82b1f53ccb37a18af58ee
Anna Prusakova <43144883+AnnaPrusakova@users.noreply.github.com>
2021-09-01 10:52:13 +0300

    Feature/216 session page (#172)
    * add session page
    
    * add session menu to addon page
    
    * edit styles

--- slidez-fe-core/src/containers/poll-editor/Menu.tsx
+++ slidez-fe-core/src/containers/poll-editor/Menu.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import menu_icon from '../../assets/svgs/menu-icon.svg'
 import check from '../../assets/svgs/check.svg'
 import q_and_a from '../../assets/svgs/QandA.svg'
 import heart from '../../assets/svgs/reactions.svg'
@@ -9,6 +8,8 @@ import './Menu.scss'
 import { useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '../../store'
+import { useState } from 'react'
+import Session from '../session/Session'
 
 const Menu: React.FC = () => {
     const dispatch = useDispatch()
@@ -19,39 +20,39 @@ const Menu: React.FC = () => {
     const { polls } = useSelector((state: RootState) => state.editor)
     return (
         <div className='main-page-addon'>
-            <div className='addon-menu'>
-                <img src={menu_icon} alt='menu' />
-            </div>
-            <h2>Create new interaction</h2>
-            <div className='menu-list'>
-                <button className='list-items' onClick={handlePollClick}>
-                    <img src={check} alt='check' />
-                    <div className='text'>Live poll</div>
-                </button>
-                <div className='list-elements'>
-                    {polls.map((item) => {
-                        return (
-                            <div className='list-items' key={item.id}>
-                                <img src={check} alt='check' />
-                                <div className='text'>{item.title}</div>
-                                &nbsp;
-                                <span className='subtext'>{`[${item.pollOptions.length} options]`}</span>
-                            </div>
-                        )
-                    })}
+            <Session />
+            <div className='addon-components'>
+                <h2>Create new interaction</h2>
+                <div className='menu-list'>
+                    <button className='list-items' onClick={handlePollClick}>
+                        <img src={check} alt='check' />
+                        <div className='text'>Live poll</div>
+                    </button>
+                    <div className='list-elements'>
+                        {polls.map((item) => {
+                            return (
+                                <div className='list-items' key={item.id}>
+                                    <img src={check} alt='check' />
+                                    <div className='text'>{item.title}</div>
+                                    &nbsp;
+                                    <span className='subtext'>{`[${item.pollOptions.length} options]`}</span>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <button className='list-items'>
+                        <img src={q_and_a} alt='q_and_a' />
+                        <div className='text'>Audience Q&#38;A</div>
+                    </button>
+                    <button className='list-items'>
+                        <img src={heart} alt='heart' />
+                        <div className='text'>Reactions</div>
+                    </button>
+                    <button className='list-items'>
+                        <img src={chat} alt='chat' />
+                        <div className='text'>Quiz</div>
+                    </button>
                 </div>
-                <button className='list-items'>
-                    <img src={q_and_a} alt='q_and_a' />
-                    <div className='text'>Audience Q&#38;A</div>
-                </button>
-                <button className='list-items'>
-                    <img src={heart} alt='heart' />
-                    <div className='text'>Reactions</div>
-                </button>
-                <button className='list-items'>
-                    <img src={chat} alt='chat' />
-                    <div className='text'>Quiz</div>
-                </button>
             </div>
         </div>
     )
---
+++ slidez-fe-core/src/containers/session/Session.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import CloseIcon from '@material-ui/icons/Close'
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos'
+import reset_button from '../../assets/svgs/reset_button.svg'
+import pause_button from '../../assets/svgs/pause_button.svg'
+import log_out from '../../assets/svgs/log_out.svg'
+import './session.scss'
+import { logout } from '../user/store'
+import { useAppDispatch } from '../../hooks'
+import { useState } from 'react'
+import menu_icon from '../../assets/svgs/menu-icon.svg'
+
+const Session = () => {
+    const dispatch = useAppDispatch()
+    const [openMenu, setOpenMenu] = useState(false)
+    const session_name = 'Determine Interfaces | Describe'
+    const session_id = '#131324235'
+
+    const sideMenuClasses = openMenu ? 'session-page-active' : 'session-page'
+
+    const handleLogout = () => {
+        dispatch(logout())
+    }
+
+    const handleCloseMenu = () => {
+        setOpenMenu(!openMenu)
+    }
+    const handleIcon = openMenu ? (
+        <div className='close-button-active'>
+            <button className='close-button' onClick={handleCloseMenu}>
+                <CloseIcon style={{ fontSize: 'large' }} />
+            </button>
+        </div>
+    ) : (
+        <div className='addon-menu'>
+            <button onClick={handleCloseMenu}>
+                <img src={menu_icon} alt='menu' />
+            </button>
+        </div>
+    )
+
+    return (
+        <div>
+            {handleIcon}
+            <div className={sideMenuClasses}>
+                <div className='session-section'>
+                    <div className='session-text'>
+                        <div className='session-name'>{session_name}</div>
+                        <div className='session-id'>{session_id}</div>
+                    </div>
+                    <button className='arrow-back'>
+                        <ArrowForwardIosIcon style={{ fontSize: 'small' }} />
+                    </button>
+                </div>
+                <hr className='border-between-button' />
+                <div className='buttons-session-section'>
+                    <button className='reset-button'>
+                        <img src={reset_button} />
+                        Reset session
+                    </button>
+                    <button className='pause-button'>
+                        <img src={pause_button} />
+                        Pause session
+                    </button>
+                </div>
+                <hr className='border-between-account' />
+                <div className='account-section'>
+                    <button className='logout' onClick={handleLogout}>
+                        <img src={log_out} />
+                        Log out
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Session
