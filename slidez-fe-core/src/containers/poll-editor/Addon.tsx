e06a0550dd756f95cc64efbc08af848ec3b03d11
DimaKosiczenko <83491348+DimaKosiczenko@users.noreply.github.com>
2021-09-02 14:21:48 +0300

    Fix Like button

--- slidez-fe-core/src/containers/poll-editor/Addon.tsx
+++ slidez-fe-core/src/containers/poll-editor/Addon.tsx
@@ -37,9 +37,9 @@ const Addon = () => {
         return 'Connecting to extension, please wait...'
     }
 
-    let tab: JSX.Element
+    let tab: JSX.Element = <Menu />
     switch (activeTab) {
-        case null:
+        case null || EditorTab.MENU:
             tab = <Menu />
             break
         case EditorTab.POLL:
