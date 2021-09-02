c24722df0715d62c7652ea7ecc2de05de267f5ef
DimaKosiczenko <83491348+DimaKosiczenko@users.noreply.github.com>
2021-09-02 14:19:24 +0300

    Add the Loader component

--- slidez-fe-addon/src/client/sidebar-add-on/App.tsx
+++ slidez-fe-addon/src/client/sidebar-add-on/App.tsx
@@ -1,28 +1,78 @@
-import React, { useEffect, useState } from 'react';
-import { Provider } from 'react-redux';
-import store from './store';
-import { EventBusConnectionStatus, useEventBus } from './hooks';
-import { EXTENSION_ID, IFRAME_HOST } from './env';
-import { runGoogleScript } from './helpers';
+import React, { useEffect, useState } from 'react'
+import { Provider } from 'react-redux'
+import store from './store'
+import { EventBusConnectionStatus, useEventBus } from './hooks'
+import { EXTENSION_ID, IFRAME_HOST } from './env'
+import { runGoogleScript } from './helpers'
+import Loader from 'slidez-fe-core/src/common/components/loader/Loader'
+import './App.scss'
 
 const App = () => {
-    const eventBus = useEventBus();
-    const [presentationId, setPresentationId] = useState<string | null>(null);
+    const eventBus = useEventBus()
+    const [presentationId, setPresentationId] = useState<string | null>(null)
     useEffect(() => {
         runGoogleScript<string>('getPresentationId', null).then(
             setPresentationId
-        );
-    });
+        )
+    })
     if (eventBus.connected === EventBusConnectionStatus.FAILED) {
         return (
-            <>Failed to connect to extension, please install extension first</>
-        );
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    flexGrow: 1,
+                    minHeight: '600px',
+                    justifyContent: 'center',
+                    textAlign: 'center',
+                    width: '100%',
+                    height: '100%',
+                    overflow: 'hidden',
+                }}
+            >
+                Failed to connect to extension, please install extension first
+            </div>
+        )
     }
     if (eventBus.connected !== EventBusConnectionStatus.CONNECTED) {
-        return <>Connecting to extension, please wait...</>;
+        return (
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    flexGrow: 1,
+                    minHeight: '600px',
+                    justifyContent: 'center',
+                    textAlign: 'center',
+                    width: '100%',
+                    height: '100%',
+                    overflow: 'hidden',
+                }}
+            >
+                <Loader />
+                Connecting to extension, please wait..
+            </div>
+        )
     }
     if (!presentationId) {
-        return <>Loading presentation info</>;
+        return (
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    flexGrow: 1,
+                    minHeight: '600px',
+                    justifyContent: 'center',
+                    textAlign: 'center',
+                    width: '100%',
+                    height: '100%',
+                    overflow: 'hidden',
+                }}
+            >
+                <Loader />
+                Connecting to extension, please wait....
+            </div>
+        )
     }
     return (
         <div
@@ -44,7 +94,7 @@ const App = () => {
                 src={`${IFRAME_HOST}/#/addon?presentationId=${presentationId}&extensionId=${EXTENSION_ID}`}
             ></iframe>
         </div>
-    );
-};
+    )
+}
 
-export default App;
+export default App
