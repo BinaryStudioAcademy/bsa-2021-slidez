f8abd92aa3a6d0468a80adba3248bd69a8563856
VolodymyrYavorskyi <83591768+VolodymyrYavorskyi@users.noreply.github.com>
2021-09-02 21:06:44 +0300

    Style slide on poll add from addon

---
+++ slidez-fe-addon/src/server/slideCreateRequest.ts
@@ -0,0 +1,194 @@
+type sizeData = {
+    height: number;
+    width: number;
+};
+
+type translateData = {
+    X: number;
+    Y: number;
+};
+
+const createSlide = (objectId: string, insertionIndex: number) => {
+    return {
+        createSlide: {
+            objectId,
+            insertionIndex,
+        },
+    };
+};
+
+const createShape = (
+    shapeId: string,
+    objectId: string,
+    sizeData: sizeData,
+    translateData: translateData
+) => {
+    return {
+        createShape: {
+            objectId: shapeId,
+            shapeType: 'TEXT_BOX',
+            elementProperties: {
+                pageObjectId: objectId,
+                size: {
+                    height: { magnitude: sizeData.height, unit: 'PT' },
+                    width: { magnitude: sizeData.width, unit: 'PT' },
+                },
+                transform: {
+                    scaleX: 1,
+                    scaleY: 1,
+                    translateX: translateData.X,
+                    translateY: translateData.Y,
+                    unit: 'PT',
+                },
+            },
+        },
+    };
+};
+
+const createTitleShape = (textboxId: string, objectId: string) => {
+    return createShape(
+        textboxId,
+        objectId,
+        { height: 150, width: 450 },
+        { X: 75, Y: 100 }
+    );
+};
+
+const createInfoShape = (infoId: string, objectId: string) => {
+    return createShape(
+        infoId,
+        objectId,
+        { height: 25, width: 300 },
+        { X: 150, Y: 325 }
+    );
+};
+
+const centerShapeText = (shapeId: string) => {
+    return {
+        updateShapeProperties: {
+            objectId: shapeId,
+            shapeProperties: {
+                contentAlignment: 'MIDDLE',
+            },
+            fields: 'contentAlignment',
+        },
+    };
+};
+
+const insertText = (shapeId: string, text: string) => {
+    return {
+        insertText: {
+            objectId: shapeId,
+            text: text,
+        },
+    };
+};
+
+const createTitle = (textboxId: string, title: string) => {
+    return insertText(textboxId, title);
+};
+
+const createInfo = (infoId: string) => {
+    return insertText(infoId, 'Start present mode to enable this slide');
+};
+
+const styleShapeText = (shapeId: string, fontSize: number) => {
+    return {
+        updateTextStyle: {
+            objectId: shapeId,
+            textRange: {
+                type: 'ALL',
+            },
+            style: {
+                fontFamily: 'Rubik',
+                fontSize: {
+                    magnitude: fontSize,
+                    unit: 'PT',
+                },
+            },
+            fields: 'fontFamily,fontSize',
+        },
+    };
+};
+
+const styleTitle = (shapeId: string) => {
+    return styleShapeText(shapeId, 36);
+};
+
+const styleInfo = (shapeId: string) => {
+    return styleShapeText(shapeId, 14);
+};
+
+const createImage = (
+    objectId: string,
+    imgUrl: string,
+    sizeData: sizeData,
+    translateData: translateData
+) => {
+    return {
+        createImage: {
+            url: imgUrl,
+            elementProperties: {
+                pageObjectId: objectId,
+                size: {
+                    height: { magnitude: sizeData.height, unit: 'PT' },
+                    width: { magnitude: sizeData.width, unit: 'PT' },
+                },
+                transform: {
+                    scaleX: 1,
+                    scaleY: 1,
+                    translateX: translateData.X,
+                    translateY: translateData.Y,
+                    unit: 'PT',
+                },
+            },
+        },
+    };
+};
+
+const createLogo = (objectId: string) => {
+    return createImage(
+        objectId,
+        'https://dbn5hoh39c7l.cloudfront.net/assets/interactive-wrapper-logo.png',
+        { height: 125, width: 125 },
+        { X: 550, Y: 25 }
+    );
+};
+
+const createIcon = (objectId: string) => {
+    return createImage(
+        objectId,
+        'https://dbn5hoh39c7l.cloudfront.net/assets/qr-code.png',
+        { height: 125, width: 125 },
+        { X: 550, Y: 250 }
+    );
+};
+
+export const sendCreateSlideRequest = (
+    presentationId: string,
+    objectId: string,
+    insertionIndex: number,
+    title: string
+) => {
+    const textboxId = `title_${Date.now().toString()}`;
+    const infoId = `info_${Date.now().toString()}`;
+
+    Slides.Presentations!.batchUpdate(
+        {
+            requests: [
+                createSlide(objectId, insertionIndex),
+                createTitleShape(textboxId, objectId),
+                createTitle(textboxId, title),
+                centerShapeText(textboxId),
+                styleTitle(textboxId),
+                createInfoShape(infoId, objectId),
+                createInfo(infoId),
+                centerShapeText(infoId),
+                styleInfo(infoId),
+                createLogo(objectId),
+                createIcon(objectId),
+            ],
+        },
+        presentationId
+    );
+};
--- slidez-fe-addon/src/server/slides.ts
+++ slidez-fe-addon/src/server/slides.ts
@@ -2,61 +2,87 @@ import {
     InsertSlideRequest,
     InsertSlideRequestSuccess,
 } from '../../../slidez-shared/src/event-bus';
+import { sendCreateSlideRequest } from './slideCreateRequest';
+
+export const getPresentation = () => {
+    return SlidesApp.getActivePresentation();
+};
 
 export const getPresentationId = () => {
-    return SlidesApp.getActivePresentation().getId();
+    return getPresentation().getId();
 };
 
 export const insertText = (text: string) => {
-    const slide = SlidesApp.getActivePresentation().getSlides()[0];
+    var slide = getPresentation().getSlides()[0];
     slide.insertTextBox(text);
 };
 
 export const insertSlide = (
     data: InsertSlideRequest
 ): InsertSlideRequestSuccess => {
-    const textboxId = `title_${Date.now().toString()}`;
-    const pt350 = { magnitude: 350, unit: 'pt' };
-    var currentPage = SlidesApp.getActivePresentation().getSelection().getCurrentPage().asSlide();
-    var indexCurrentPage = SlidesApp.getActivePresentation().getSlides().findIndex(x => x.getObjectId() == currentPage.getObjectId());
-    var nextSlide = indexCurrentPage + 1;
-    Slides.Presentations!.batchUpdate(
-        {
-            requests: [
-                {
-                    createSlide: {
-                        objectId: data.id,
-                        insertionIndex: nextSlide,
-                    },
-                },
-                {
-                    createShape: {
-                        objectId: textboxId,
-                        shapeType: 'TEXT_BOX',
-                        elementProperties: {
-                            pageObjectId: data.id,
-                            size: { height: pt350, width: pt350 },
-                            transform: {
-                                unit: 'pt',
-                                scaleX: 1,
-                                scaleY: 1,
-                                translateX: 10,
-                                translateY: 30,
-                            },
-                        },
-                    },
-                },
-                {
-                    insertText: {
-                        objectId: textboxId,
-                        text: data.title,
-                    },
-                },
-            ],
-        },
-        SlidesApp.getActivePresentation().getId()
+    var selection = getPresentation().getSelection();
+    var currentPage = selection.getCurrentPage();
+    var insertionIndex =
+        getPresentation()
+            .getSlides()
+            .findIndex(x => x.getObjectId() == currentPage.getObjectId()) + 1;
+
+    sendCreateSlideRequest(
+        getPresentationId(),
+        data.id,
+        insertionIndex,
+        data.title
     );
-    // example to focus another slide
-    // SlidesApp.getActivePresentation().getSlides()[1].selectAsCurrentPage()
+
     return { insertedId: data.id };
 };
+
+// export const insertSlide = (
+//     data: InsertSlideRequest
+// ): InsertSlideRequestSuccess => {
+//     const textboxId = `title_${Date.now().toString()}`;
+//     const pt350 = { magnitude: 350, unit: 'pt' };
+//     const presentation = getPresentation()
+//     var currentPage = presentation.getSelection().getCurrentPage().asSlide();
+//     var indexCurrentPage = presentation.getSlides().findIndex(x => x.getObjectId() == currentPage.getObjectId());
+//     var nextSlide = indexCurrentPage + 1;
+// Slides.Presentations!.batchUpdate(
+//     {
+//         requests: [
+//             {
+//                 createSlide: {
+//                     objectId: data.id,
+//                     insertionIndex: nextSlide,
+//                 },
+//             },
+//             {
+//                 createShape: {
+//                     objectId: textboxId,
+//                     shapeType: 'TEXT_BOX',
+//                     elementProperties: {
+//                         pageObjectId: data.id,
+//                         size: { height: pt350, width: pt350 },
+//                         transform: {
+//                             unit: 'pt',
+//                             scaleX: 1,
+//                             scaleY: 1,
+//                             translateX: 10,
+//                             translateY: 30,
+//                         },
+//                     },
+//                 },
+//             },
+//             {
+//                 insertText: {
+//                     objectId: textboxId,
+//                     text: data.title,
+//                 },
+//             },
+//         ],
+//     },
+//     getPresentationId()
+// );
+//     // example to focus another slide
+//     // SlidesApp.getActivePresentation().getSlides()[1].selectAsCurrentPage()
+//     return { insertedId: data.id };
+// };
