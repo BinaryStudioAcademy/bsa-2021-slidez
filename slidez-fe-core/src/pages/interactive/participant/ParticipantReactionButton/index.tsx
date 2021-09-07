539dfa1534b6072142d63fe330672f47447340cc
Anatolii Ivanov <anatolii-ivanov@binary-studio.com>
2021-09-07 22:42:34 +0300

    Style reactions a bit on participant page

--- slidez-fe-core/src/pages/interactive/participant/ParticipantReactionButton/index.tsx
+++ slidez-fe-core/src/pages/interactive/participant/ParticipantReactionButton/index.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { createAddReactionEvent } from '../../../../containers/session/event/FrontendEvent'
 import { addReaction } from '../../../../containers/session/store/store'
 import { Reactions } from '../../../../types/reactions'
+import styles from './styles.module.scss'
+import { ReactComponent as HeartIcon } from '../../../../assets/svgs/reactions/heart.svg'
+import { ReactComponent as ThumbUpIcon } from '../../../../assets/svgs/reactions/thumbUp.svg'
+import { useDetectOutsideClick } from '../../../../pages/dashboard/useDetectOutsideClick'
 
 export type ParticipantReactionProps = {
     link: string
@@ -15,7 +19,6 @@ const makeReactionHandler =
     }
 
 const ParticipantReactionButton: React.FC<ParticipantReactionProps> = ({
-    // eslint-disable-next-line react/prop-types
     link,
 }) => {
     const dispatch = useDispatch()
@@ -27,11 +30,46 @@ const ParticipantReactionButton: React.FC<ParticipantReactionProps> = ({
         makeReactionHandler(dispatch, link, Reactions.LOVE, ''),
         [dispatch, link]
     )
+
+    const openedBlockRef = useRef<HTMLInputElement>(null)
+    const [isOpened, setIsOpened] = useDetectOutsideClick(openedBlockRef, false)
+    const isDesktop = window.innerWidth > 800
+
+    const toggleOpenClick = () => {
+        openedBlockRef.current?.blur()
+        setIsOpened(true)
+    }
+
+    const defaultIconSize = 20
     //TODO: Add disabling input for one minute after sending a reaction
     return (
-        <div>
-            <button onClick={handleLikeClick}>👍</button>
-            <button onClick={handleLoveClick}>❤️</button>
+        <div className={styles.reactions}>
+            {isOpened || isDesktop ? (
+                <div className={styles.openedBlock} ref={openedBlockRef}>
+                    <button onClick={handleLikeClick}>
+                        <ThumbUpIcon
+                            width={defaultIconSize}
+                            height={defaultIconSize}
+                        />
+                    </button>
+                    <button onClick={handleLoveClick}>
+                        <HeartIcon
+                            width={defaultIconSize}
+                            height={defaultIconSize}
+                        />
+                    </button>
+                </div>
+            ) : (
+                ''
+            )}
+            <div
+                className={`${styles.closedBlock} ${
+                    isOpened || isDesktop ? styles.hidden : ''
+                }`}
+                onClick={toggleOpenClick}
+            >
+                <HeartIcon width={defaultIconSize} height={defaultIconSize} />
+            </div>
         </div>
     )
 }
