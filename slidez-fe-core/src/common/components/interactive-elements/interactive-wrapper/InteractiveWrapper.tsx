a68c845806f27f143ec4700409ef6bf5d2a9bb99
DimaKosiczenko <83491348+DimaKosiczenko@users.noreply.github.com>
2021-09-07 12:06:26 +0300

    After answer is submitted it shouldn't be possible to change it in poll

--- slidez-fe-core/src/common/components/interactive-elements/interactive-wrapper/InteractiveWrapper.tsx
+++ slidez-fe-core/src/common/components/interactive-elements/interactive-wrapper/InteractiveWrapper.tsx
@@ -17,8 +17,8 @@ const InteractiveWrapper: React.FC<InteractiveWrapperProps> = ({
         <div className='wrapper'>
             <div className='wrapped-component'>{children}</div>
             <div className='interactive-wrapper-sidebar'>
-                <div className='container-with-centered-content'>
-                    <InteractiveLogo width='100%' height='350px' />
+                <div className='logo-holder container-with-centered-content'>
+                    <InteractiveLogo width='100%' height='150px' />
                 </div>
                 <div className='link-holder'>
                     Join at
