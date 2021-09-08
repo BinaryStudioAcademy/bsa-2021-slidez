390026d91f2dbfe794f27e088b6bc4876cf758ec
Anna Prusakova <hanna.prusakova@gmail.com>
2021-09-08 13:22:26 +0300

    add link to event

--- slidez-fe-core/src/pages/interactive/participant/EventItem.tsx
+++ slidez-fe-core/src/pages/interactive/participant/EventItem.tsx
@@ -5,8 +5,9 @@ import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
 import moment from 'moment'
 import { AppRoute } from '../../../common/routes/app-route'
 import { ParticipantEvent } from '../../../services/participant-event/dto/ParticipantEvent'
+import { Link } from 'react-router-dom'
 
-const createEndpoint = (code: string) => `/#${AppRoute.EVENTS}/${code}`
+const createEndpoint = (code: string) => `${AppRoute.EVENTS}/${code}`
 
 const lastViewsDate = (date: Date) => {
     const diffDate = moment(date).fromNow()
@@ -19,19 +20,24 @@ export type EventItemProps = {
 
 export const EventItem = ({ event }: EventItemProps) => {
     return (
-        <div className='event-item'>
-            <div className='event-info'>
-                <div className='event-info-text'>
-                    <div className='event-info-name'>{event.name}</div>
-                    <div className='event-code'>#{event.code}</div>
+        <Link to={createEndpoint(event.code)}>
+            <div className='event-item'>
+                <div className='event-info'>
+                    <div className='event-info-text'>
+                        <div className='event-info-name'>{event.name}</div>
+                        <div className='event-code'>#{event.code}</div>
+                    </div>
+                    <div className='event-info-viewsDate'>
+                        {lastViewsDate(event.viewsDate)}
+                    </div>
                 </div>
-                <div className='event-info-viewsDate'>
-                    {lastViewsDate(event.viewsDate)}
-                </div>
+                <a className='event-icon'>
+                    <FontAwesomeIcon
+                        className='angle-right'
+                        icon={faAngleRight}
+                    />
+                </a>
             </div>
-            <a className='event-icon' href={createEndpoint(event.code)}>
-                <FontAwesomeIcon className='angle-right' icon={faAngleRight} />
-            </a>
-        </div>
+        </Link>
     )
 }
