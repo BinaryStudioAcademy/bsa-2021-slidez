ab4484b8eaa751db19f5aeca231e2367d76aacfb
Anna Prusakova <hanna.prusakova@gmail.com>
2021-09-08 11:48:09 +0300

    add userName dialog to Header

--- slidez-fe-core/src/pages/interactive/Header.tsx
+++ slidez-fe-core/src/pages/interactive/Header.tsx
@@ -3,15 +3,23 @@ import './header.scss'
 import SlidezLogo from '../../logo_Slidez_black.svg'
 import { Link } from 'react-router-dom'
 import { AppRoute } from '../../common/routes/app-route'
+import { ParticipantData } from '../../services/participant/dto/ParticipantData'
+import { getParticipantData } from '../../services/participant/participant-service'
+import ParticipantNameDialog from './participant/ParticipantNameModal/ParticipantNameModal'
 
 interface IProps {
     eventName: string
 }
 
 const Header: React.FC<IProps> = ({ eventName }: IProps) => {
+    const participantData: ParticipantData = getParticipantData()
+    const areFirstAndLastNamePresent =
+        participantData.participantFirstName &&
+        participantData.participantLastName
     return (
         <div className='participant-header'>
             <div className='logo'>
+                {!areFirstAndLastNamePresent ? <ParticipantNameDialog /> : ''}
                 <Link to={AppRoute.EVENTS}>
                     <img src={SlidezLogo} alt='Slidez Logo' />
                 </Link>
--- slidez-fe-core/src/pages/interactive/participant/ParticipantPage.tsx
+++ slidez-fe-core/src/pages/interactive/participant/ParticipantPage.tsx
@@ -1,23 +1,15 @@
 import React from 'react'
 import './participantPage.scss'
 import Header from '../Header'
-import ParticipantNameDialog from './ParticipantNameModal/ParticipantNameModal'
-import { ParticipantData } from '../../../services/participant/dto/ParticipantData'
-import { getParticipantData } from '../../../services/participant/participant-service'
 import { EventItem } from './EventItem'
 import { getParticipantEvents } from '../../../services/participant-event/participant-event-service'
 
 const ParticipantPage = () => {
     const participantEvents = getParticipantEvents()
-    const participantData: ParticipantData = getParticipantData()
-    const areFirstAndLastNamePresent =
-        participantData.participantFirstName &&
-        participantData.participantLastName
 
     return (
         <div className='participant-page'>
             <Header eventName='' />
-            {!areFirstAndLastNamePresent ? <ParticipantNameDialog /> : ''}
             <div className='input-block'>
                 <div>Enter code</div>
                 <input className='code-input' type='text' placeholder='#Code' />
