2b69159d5bad3b0aaca321c28a7bda2cdea85093
VladislavArzamastsev <68342736+VladislavArzamastsev@users.noreply.github.com>
2021-09-09 17:48:45 +0300

    Fixed blinking dashboard issue (#251)

--- slidez-fe-core/src/services/http/http-helper.ts
+++ slidez-fe-core/src/services/http/http-helper.ts
@@ -1,15 +1,10 @@
-import {
-    getAuthHeaderValue,
-    performLogout,
-    performRefreshTokens,
-} from '../auth/auth-service'
+import { getAuthHeaderValue, performRefreshTokens } from '../auth/auth-service'
 import HttpHelper from 'slidez-shared/src/net/http/http-helper'
 import { ApiGateway } from './api-gateway'
 
 const httpHelper = new HttpHelper(
     getAuthHeaderValue,
     performRefreshTokens,
-    performLogout,
     ApiGateway.REACT_APP_API_GATEWAY
 )
 
