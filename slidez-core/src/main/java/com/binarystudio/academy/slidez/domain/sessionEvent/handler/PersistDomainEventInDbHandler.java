36fb8200bdb2221a08dab64ba89eb104ecc8af40
VladyslavArzamastsev <arzik66677@gmail.com>
2021-09-09 18:23:53 +0300

    Commented bug

--- slidez-core/src/main/java/com/binarystudio/academy/slidez/domain/sessionEvent/handler/PersistDomainEventInDbHandler.java
+++ slidez-core/src/main/java/com/binarystudio/academy/slidez/domain/sessionEvent/handler/PersistDomainEventInDbHandler.java
@@ -5,15 +5,10 @@ import com.binarystudio.academy.slidez.domain.response.GenericResponse;
 import com.binarystudio.academy.slidez.domain.session.PresentationEventStore;
 import com.binarystudio.academy.slidez.domain.session.dto.SessionResponse;
 import com.binarystudio.academy.slidez.domain.session.event.DomainEvent;
-import com.binarystudio.academy.slidez.domain.session.event.SnapshotRequestedEvent;
 import com.binarystudio.academy.slidez.domain.session.handler.AbstractDomainEventHandler;
 import com.binarystudio.academy.slidez.domain.sessionEvent.SessionEventService;
-import com.binarystudio.academy.slidez.domain.sessionEvent.model.SessionEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.Objects;
 
 @Component
 public class PersistDomainEventInDbHandler extends AbstractDomainEventHandler {
@@ -28,9 +23,10 @@ public class PersistDomainEventInDbHandler extends AbstractDomainEventHandler {
 	@Override
 	public GenericResponse<SessionResponse, SessionResponseCodes> handle(DomainEvent domainEvent,
 			PresentationEventStore presentationEventStore) {
-		if (!Objects.equals(domainEvent.getClass(), SnapshotRequestedEvent.class)) {
-			sessionEventService.create(presentationEventStore.getPresentationLink(), domainEvent);
-		}
+		// if (!Objects.equals(domainEvent.getClass(), SnapshotRequestedEvent.class)) {
+		// sessionEventService.create(presentationEventStore.getPresentationLink(),
+		// domainEvent);
+		// }
 		return super.handle(domainEvent, presentationEventStore);
 	}
 
